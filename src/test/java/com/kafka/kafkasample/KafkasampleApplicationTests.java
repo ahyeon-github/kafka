88e49e7031215b68574a263efe209774aeed1b4d
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2024-10-08 11:31:23 +0900

    springboot kafka 연동

---
+++ src/test/java/com/kafka/kafkasample/KafkasampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.kafkasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkasampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
